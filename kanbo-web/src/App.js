ad0d043c3e2ab5ae8614d2d7a34185de6149a0b5
Yovanta Anjelina <yovantaanjelina1001@gmail.com>
2022-06-01 23:36:56 +0700

    feat: add verification page

--- kanbo-web/src/App.js
+++ kanbo-web/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import './App.css';
 import Register from './Pages/Register';
+import Verification from './Pages/Verification';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="App">
         <Routes>
             <Route path='/register' element={<Register/>}/>
+            <Route path='/verify' element={<Verification/>}/>
         </Routes>
       </div>
     </Router>
